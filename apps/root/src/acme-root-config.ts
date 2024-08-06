7898163a83afdba54b669cf6e08e86179aded79f
Mehmet Yarar <Mehmet.Yarar@vertexinc.com>
2024-08-06 16:36:04 +0500

    add mf-chrome

--- apps/root/src/acme-root-config.ts
+++ apps/root/src/acme-root-config.ts
@@ -1,19 +1,25 @@
 import { registerApplication, start, LifeCycles } from "single-spa";
 
-registerApplication({
-  name: "@single-spa/welcome",
-  app: () =>
-    System.import<LifeCycles>(
-      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-    ),
-  activeWhen: ["/"],
-});
+// registerApplication({
+//   name: "@single-spa/welcome",
+//   app: () =>
+//     System.import<LifeCycles>(
+//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+//     ),
+//   activeWhen: ["/"],
+// });
 
 // registerApplication({
 //   name: "@acme/navbar",
 //   app: () => System.import("@acme/navbar"),
 //   activeWhen: ["/"]
 // });
+
+registerApplication({
+  name: "@acme/mf-chrome",
+  app: () => System.import<LifeCycles>("@acme/mf-chrome"),
+  activeWhen: ["/"],
+});
 
 start({
   urlRerouteOnly: true,
